a6aa4093f71e6811bc9223eaace2a3545f8c456a
lolson5858 <109360413+lolson5858@users.noreply.github.com>
2022-09-21 12:05:16 -0500

    js card animation on click expansion, text transition

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// using querySelectorAll creates an Nodelist
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+      removeActiveClasses()
+      panel.classList.add('active');
+    }) 
+})
+
+function removeActiveClasses () {
+  panels.forEach(panel => {
+      panel.classList.remove('active');
+  })
+}
